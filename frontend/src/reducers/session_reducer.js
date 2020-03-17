3fdbc1fa8a03a76a14a86641e88572c86960ef6a
terrychow <terryhchow@gmail.com>
2020-03-16 23:17:18 -0700

    added tweets

---
+++ frontend/src/reducers/session_reducer.js
@@ -0,0 +1,16 @@
+const initialState = {
+  isAuthenticated: false,
+  user: {}
+};
+
+export default function(state = initialState, action) {
+  switch (action.type) {
+    case RECEIVE_USER_LOGOUT:
+      return {
+        isAuthenticated: false,
+        user: undefined
+      };
+    default:
+      return state;
+  }
+}
